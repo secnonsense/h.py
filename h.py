5fc6866c6701fc823f63f159f64e2514ae6221d4
Scott Winters <swinters@tns4662l.local>
2017-08-18 13:11:23 -0400

    formatting changes

--- h.py
+++ h.py
@@ -13,16 +13,19 @@ args = parser.parse_args()
 x=1
 filename = ''
 host = args.URL.split('/')
+
 for x in range (1, len(host), 1):
         filename = filename+'/'+host[x]
         x=x+1
 method = 'GET'
 headers = {"Accept": "image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, application/vnd.ms-powerpoint, application/vnd.ms-excel, application/msword, application/x-shockwave-flash, */*", "Accept-Language": "en-us", "User-Agent": "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727)", "Connection": "Keep-Alive"}
+
 if args.ssl:
     conn = httplib.HTTPSConnection(host[0])
 else:
     conn = httplib.HTTPConnection(host[0])	
 conn.request(method, filename, None, headers)
+
 httpResponse = conn.getresponse() 
 if args.status:
     print "status: ", httpResponse.status 
